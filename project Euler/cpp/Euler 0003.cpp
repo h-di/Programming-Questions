88de9759e24587df9f5f83d3ea973142db35b807
hadiPC <hadi@pc>
2020-07-04 08:43:37 +0430

    Euler 3 was solved ✔

--- project Euler/cpp/Euler 0003.cpp
+++ project Euler/cpp/Euler 0003.cpp
@@ -2,3 +2,35 @@
 // Created by hadi on ۰۴/۰۷/۲۰۲۰.
 //
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    auto n = 600851475143;
+
+    int LargestFactor = 0;
+    int i = 2;
+
+
+    while (i * i <= n) {
+
+        if (n % i == 0) {
+
+            n /= i;
+            LargestFactor = i;
+
+        } else
+            i++;
+    }
+
+    if (n > LargestFactor)
+        LargestFactor = n;
+
+
+    cout << LargestFactor;
+
+
+    return 0;
+}
